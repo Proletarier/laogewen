c7c9c8379782a4f10bc50f69a8a446550589a899
Heng Wen <767798017@qq.com>
2018-03-23 18:04:13 +0800

    first

---
+++ src/main/java/com/app/mvc/acl/filter/LoginFilter.java
@@ -0,0 +1,27 @@
+package com.app.mvc.acl.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2018/3/23.
+ */
+public class LoginFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
